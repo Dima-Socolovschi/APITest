5cb055605cf84d72d648cf1113eb7659f6ddad1b
DSocolovschi <d.socolovschi3@gmail.com>
2024-05-21 17:09:27 +0300

    First Commit

--- src/test/java/tests/TestRequests.java
+++ src/test/java/tests/TestRequests.java
@@ -1,2 +1,75 @@
-package tests;public class TestRequests {
+package tests;
+
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static utils.Steps.*;
+
+public class TestRequests {
+
+    @BeforeSuite
+    public void setUp() {
+        baseURI = "https://reqres.in/api";
+    }
+
+    @Test
+    public void testListUsers() {
+        String url = "/users?page=2";
+        Response response = GET(url);
+        isStatusCodeValid(response, 200);
+    }
+
+    @Test
+    public void testSingleUser() {
+        String url = "/users/2";
+        Response response = GET(url);
+        isStatusCodeValid(response, 200);
+        isBodyContains(response, "id");
+    }
+
+    @Test
+    public void testCreate() {
+        String url = "/users";
+        String body = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"leader\"\n" +
+                "}";
+        Response response = POST(body, url);
+        isStatusCodeValid(response, 201);
+        isBodyContains(response, "id");
+    }
+
+    @Test
+    public void testUpdatePut() {
+        String url = "/users/2";
+        String body = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"zion resident\"\n" +
+                "}";
+        Response response = PUT(body, url);
+        isStatusCodeValid(response, 200);
+        isBodyContains(response, "updatedAt");
+    }
+
+    @Test
+    public void testUpdatePatch() {
+        String url = "/users/2";
+        String body = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"zion resident\"\n" +
+                "}";
+        Response response = PATCH(body, url);
+        isStatusCodeValid(response, 200);
+        isBodyContains(response, "updatedAt");
+    }
+
+    @Test
+    public void testDelete(){
+        String url = "/users/2";
+        Response response = DELETE(url);
+        isStatusCodeValid(response, 204);
+    }
+
 }
+ 
